e00f37e18fa7765a27af4d1336733a0f80c36f1f
Brody Losh <brodylosh@gmail.com>
2022-04-06 16:18:23 -0400

    commit tree e06116390ff0cc702bf27508165398b31c12f07b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Brody",
+    streetAddress: "123 Main St.",
+}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    let newObj = {...employee}
+    newObj[key] = value;
+    return newObj;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    let newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+}
